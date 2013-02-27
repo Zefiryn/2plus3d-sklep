352df223e824ac561724384285653076423ca911
Zefiryn <zefiryn@jewula.net>
2013-02-27 22:07:36 +0100

    changes to the product layout

--- app/code/local/DpTD/Catalog/Helper/Data.php
+++ app/code/local/DpTD/Catalog/Helper/Data.php
@@ -1,7 +1,35 @@
 <?php
 class DpTD_Catalog_Helper_Data extends Mage_Catalog_Helper_Data
 {
+  protected $_categories = array();
+
   public function getFormatedDate($date) {
     return Mage::helper('site')->ago($date);
+  }
+
+  public function isInCategory($product, $categoryKey) {
+    $include = false;
+    foreach($product->getCategoryIds() as $categoryId) {
+      if ($this->_getCategoryName($categoryId) == $categoryKey) {
+        $include = true;
+      }
+    }
+    return $include;
+  }
+
+  public function getProductCategoryName($product) {
+    if ($this->isInCategory($product, 'ksiazki')) {
+      return 'Książki';
+    }
+    if ($this->isInCategory($product, 'kwartalnik')) {
+      return 'Kwartalnik';
+    }
+  }
+
+  protected function _getCategoryName($categoryId) {
+    if ($this->_categories[$categoryId] == null) {
+      $this->_categories[$categoryId] = Mage::getModel('catalog/category')->load($categoryId)->getUrlKey();
+    }
+    return $this->_categories[$categoryId];
   }
 }
