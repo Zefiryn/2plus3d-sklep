59a0e5ac07bf55b9816256079cb6fea09d1397b8
Zefiryn <zefiryn@jewula.net>
2013-08-23 21:07:31 +0200

    Update ifirma module

--- app/code/local/PowerMedia/Ifirma/Model/Ifirma.php
+++ app/code/local/PowerMedia/Ifirma/Model/Ifirma.php
@@ -1,12 +1,27 @@
 <?php
 
+require_once dirname(__FILE__) . '/Connector/Filters/InvoiceNumberFilter.php';
+
 /**
- * Description of AccountancyDocumentData
+ * Description of Ifirma
  *
- * @author platowski
+ * @author bbojanowicz
  */
-class PowerMedia_Ifirma_Model_Ifirma extends Mage_Core_Model_Abstract {
-	public function _construct() {
-		$this->_init('ifirma/ifirma','id');
+class PowerMedia_Ifirma_Model_Ifirma extends Mage_Core_Model_Abstract{
+	
+	const TYPE_INVOICE = 'invoice';
+	const TYPE_INVOICE_SEND = 'invoice_send';
+	const TYPE_INVOICE_PROFORMA = 'invoice_proforma';
+	const TYPE_INVOICE_BILL = 'invoice_bill';
+	
+	protected function _construct(){
+		$this->_init('ifirma/ifirma');
+	}
+	
+	public function getFilteredNumber(){
+		$filter = new ifirma\InvoiceNumberFilter();
+		
+		return $filter->filter($this->getInvoiceNumber());
 	}
 }
+
